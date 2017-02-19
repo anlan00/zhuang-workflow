706fc73e61b6173e035572ab4abe1a8a3c829283
weibin268 <binweizhuang@hotmail.com>
2017-02-19 21:17:09 +0800

    aa

--- src/main/java/com/zhuang/workflow/test/Test01ActionListener.java
+++ src/main/java/com/zhuang/workflow/test/Test01ActionListener.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.zhuang.workflow.WorkflowActionListener;
 import com.zhuang.workflow.WorkflowEngineContext;
 import com.zhuang.workflow.models.UserInfoModel;
+import com.zhuang.workflow.models.WorkflowChoiceOptions;
 
 public class Test01ActionListener implements WorkflowActionListener {
 
@@ -51,7 +52,11 @@ public class Test01ActionListener implements WorkflowActionListener {
 
 	public void onRetrieveFormData(WorkflowEngineContext context) {
 		System.out.println("onRetrieveFormData--------------------");
-			
+		System.out.println("context.getCurrentTaskDef()"+context.getCurrentTaskDef());
+		if(context.getCurrentTaskDef().equals("mgr1") || context.getCurrentTaskDef().equals("mgr2"))
+		{
+			context.getFormData().put("toolbar_back", true);
+		}
 	}
 
 	public void beforDelete(WorkflowEngineContext context) {
