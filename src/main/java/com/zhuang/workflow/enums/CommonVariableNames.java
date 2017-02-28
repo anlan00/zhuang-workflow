223951fc221e5225a83f97000e7b8d8e5360aa19
weibin268 <binweizhuang@hotmail.com>
2017-02-28 12:38:27 +0800

    aa

--- src/main/java/com/zhuang/workflow/enums/CommonVariableNames.java
+++ src/main/java/com/zhuang/workflow/enums/CommonVariableNames.java
@@ -5,5 +5,7 @@ public class CommonVariableNames {
 	public static final String HANDLER_NAME_PREFIX = "$";
 
 	public static final String NAME_VALUE_SEPARATOR = ":";
+	
+	public static final String COUNTERSIGN_USERS = "countersignUsers";
 
 }
