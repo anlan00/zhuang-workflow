90bc96ff6fecc8fbb1540e91e81c21b7223f0708
weibin268 <binweizhuang@hotmail.com>
2017-03-14 11:20:27 +0800

    aa

--- src/main/java/com/zhuang/workflow/enums/CountersignVariableNames.java
+++ src/main/java/com/zhuang/workflow/enums/CountersignVariableNames.java
@@ -4,4 +4,8 @@ public class CountersignVariableNames {
 
 	public static final String COUNTERSIGN_USERS = "countersignUsers";
 
+	public static final String COUNTERSIGN_APPROVED_COUNT = "countersignApprovedCount";
+	
+	public static final String COUNTERSIGN_REJECTED_COUNT = "countersignRejectedCount";
+	
 }
